e2337549d1a493570fa8ffde80fa9b441bd5f6b4
Justin Essert <justinkessert@gmail.com>
2025-10-31 11:50:27 -0400

    corrected import

---
+++ vacation_mcp/tools/__init__.py
@@ -0,0 +1,11 @@
+from vacation_mcp.tools.vacation import (
+    get_vacation_history,
+    get_remaining_vacation_days,
+    log_vacation_day,
+)
+
+__all__ = [
+    "get_vacation_history",
+    "get_remaining_vacation_days",
+    "log_vacation_day",
+]
